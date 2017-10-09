a53b188e432e455340e3fbe08ddd233d210ff532
Alberto Leiva <alberto.donlimpio@gmail.com>
2017-10-09 20:02:12 +0200

    Main Menu panel initialisated

---
+++ SlothWars/Assets/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	private Image panelMain;
+	private Image panelVS;
+	private Button playB;
+
+	// Use this for initialization
+	void Awake() {
+		panelMain = GameObject.Find ("mainMenuPanel").GetComponent<Image>();
+		panelVS = GameObject.Find ("playVSPanel").GetComponent<Image>();
+		playB = GameObject.Find ("playButton").GetComponent<Button>();
+		panelVS.gameObject.SetActive (false);
+		playB.onClick.AddListener (delegate{playSelected();});
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void playSelected(){
+		panelMain.gameObject.SetActive (false);
+		panelVS.gameObject.SetActive (true);
+	}
+}
