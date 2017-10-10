7aa1a87aadee89ec31ab973808729268dafdcc33
jlgz <josepu_flow@hotmail.com>
2017-10-10 13:48:38 +0200

    movimiento + health/projectile

---
+++ SlothWars/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour {
+    private int health= 100;
+    private TextMesh text;
+	// Use this for initialization
+	void Start () {
+        text = GetComponentInChildren<TextMesh>();
+        text.text = ""+ health;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void TakeDamage(int d)
+    {
+        health -= d;
+        text.text = "" + health;
+    }
+}
