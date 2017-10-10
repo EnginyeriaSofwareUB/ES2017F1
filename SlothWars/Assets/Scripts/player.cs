8c680cba92cd4b9cdb88a0c41d0303e5633bd865
Albert Segura Santillana <segu.santi@gmail.com>
2017-10-10 18:49:41 +0200

    Millores en el moviment en 2D. S'ha eliminat el moviment en profunditat perquè no l'utilitzarem.

--- SlothWars/Assets/Scripts/player.cs
+++ SlothWars/Assets/Scripts/player.cs
@@ -9,6 +9,7 @@ public class player : MonoBehaviour {
 
 	private float inputH;
 	private float inputV;
+	private int k = 0;
 
 	// Use this for initialization
 	void Start () 
@@ -20,11 +21,6 @@ public class player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown ("1")) 
-		{
-			anim.Play ("walk",-1,0f);
-		}
-
 		if (Input.GetKey (KeyCode.Space)) 
 		{
 			anim.SetBool ("jump", true);
@@ -35,20 +31,11 @@ public class player : MonoBehaviour {
 		}
 
 		inputH = Input.GetAxis ("Horizontal");
-		inputV = Input.GetAxis ("Vertical");
 
 		anim.SetFloat ("inputH", inputH);
-		anim.SetFloat ("inputV", inputV);
 
-		float moveX = inputH * 20f * Time.deltaTime;
-		float moveZ = inputV * 50f * Time.deltaTime;
+		float moveZ = inputH * 50f * Time.deltaTime;
 
-		/*if (moveZ <= 0f) 
-		{
-			moveX = 0f;
-		}*/
-
-		rbody.velocity = new Vector3 (moveX, 0f, moveZ);
-		//rbody.AddForce(new Vector3(moveX, 0f, moveZ), ForceMode.VelocityChange);
+		rbody.velocity = new Vector3 (0f, 0f, moveZ);
 	}
 }
