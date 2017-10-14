879482d869b1366a311d2f718faa4003dd983ddb
Ardaghor <jose.lamas.rodriguez@gmail.com>
2017-10-14 13:45:09 +0200

    Sistema de turnos integrado para un equipo de sloths

--- SlothWars/Assets/Scripts/ChangeTurn.cs
+++ SlothWars/Assets/Scripts/ChangeTurn.cs
@@ -6,60 +6,127 @@ public class ChangeTurn : MonoBehaviour
 {
 
     // Getting the sloths in the scene.
-    public GameObject enemySloth,gunEnemySloth;
-    public GameObject friendlySloth,gunFriendlySloth;
+    List<GameObject> slothTeamA, gunSlothTeamA;
+    List<GameObject> slothTeamB, gunSlothTeamB;
+
+    
+    //GameObject with the script where the sloths are created
+    GameObject managerTeam;
+
+    int slothTurnA, slothTurnB;
 
     // Initially, the friendly sloth has not ended his turn.
     public bool endTurnOfSloth = false;
-
+    public bool beginStopped = true;
 
     private void Start()
     {
-        enemySloth = GameObject.Find("avatarEnemySloth");
-        gunEnemySloth = GameObject.Find("gunEnemySloth");
+
+        slothTurnA = 0;
+        slothTurnB = 0;
+
+        slothTeamA = new List<GameObject>();
+        gunSlothTeamA = new List<GameObject>();
+
+        slothTeamB = new List<GameObject>();
+        gunSlothTeamB = new List<GameObject>();
+
+        managerTeam = GameObject.Find("managerTeam");
+    }
+
+    private void Update()
+    {
+        slothTeamA = managerTeam.GetComponent<CreateSloth>().teamA;
+        gunSlothTeamA = managerTeam.GetComponent<CreateSloth>().gunsTeamA;
+
+        slothTeamB = managerTeam.GetComponent<CreateSloth>().teamB;
+        gunSlothTeamB = managerTeam.GetComponent<CreateSloth>().gunsTeamB;
+
+    }
+
+
+    public void DeactivateAllExceptOne(List<GameObject> slothTeamA, List<GameObject> gunSlothTeamA, List<GameObject> slothTeamB, List<GameObject> gunSlothTeamB)
+    {
+        foreach (GameObject sloth in slothTeamB)
+        {
+            sloth.GetComponent<Animator>().enabled = false;
+            sloth.GetComponent<player>().enabled = false;
+            
+        }
+
+        foreach (GameObject gun in gunSlothTeamB)
+        {
+            gun.GetComponent<ShotScript>().enabled = false;
+        }
+
+        for (int i = 1; i<slothTeamA.Count; i++)
+        {
+            slothTeamA[i].GetComponent<Animator>().enabled = false;
+            slothTeamA[i].GetComponent<player>().enabled = false;
+        }
+
+        for (int i = 1; i < slothTeamA.Count; i++)
+        {
+            gunSlothTeamA[i].GetComponent<ShotScript>().enabled = false;
         
-       
-        friendlySloth = GameObject.Find("avatarFriendlySloth");
-        gunFriendlySloth = GameObject.Find("gunFriendlySloth");
+        }
+    }
 
+    // Functions to activate and deactivate sloth's animations
+    private void ActivateSloth(GameObject slothTeam, GameObject gunTeamSloth)
+    {
+        slothTeam.GetComponent<Animator>().enabled = true;
+        slothTeam.GetComponent<player>().enabled = true;
+        gunTeamSloth.GetComponent<ShotScript>().enabled = true;
 
 
     }
-    
-    
-    private void ActivateSloth(GameObject sloth, GameObject gunSloth)
-    {
-        sloth.GetComponent<Animator>().enabled = false;
-        sloth.GetComponent<player>().enabled = false;
-        gunSloth.GetComponent<ShotScript>().enabled = false;
 
-    }
-
-    private void DeactivateSloth(GameObject sloth, GameObject gunSloth)
+    private void DeactivateSloth(GameObject slothTeam, GameObject gunTeamSloth)
     {
-        sloth.GetComponent<Animator>().enabled = true;
-        sloth.GetComponent<player>().enabled = true;
-        gunSloth.GetComponent<ShotScript>().enabled = true;
+        
+        slothTeam.GetComponent<Animator>().enabled = false;
+        slothTeam.GetComponent<player>().enabled = false;
+        gunTeamSloth.GetComponent<ShotScript>().enabled = false;
+
+    
     }
 
     public void FinishTurn()
     {
-        // If he has ended, he will press the button, changing the variable to true.
-        if (!endTurnOfSloth)
+        if (beginStopped)
         {
-            endTurnOfSloth = true;
-            DeactivateSloth(friendlySloth, gunFriendlySloth);
-            ActivateSloth(enemySloth, gunEnemySloth);
+            DeactivateAllExceptOne(slothTeamA, gunSlothTeamA, slothTeamB, gunSlothTeamB);
+            beginStopped = false;
+            return;
+        }
 
+        // If he has ended, he will press the button, changing the variable to true.
+        if (endTurnOfSloth)
+        {
+            endTurnOfSloth = false;
+            DeactivateSloth(slothTeamB[slothTurnB], gunSlothTeamA[slothTurnB]);
+            ActivateSloth(slothTeamA[slothTurnA], gunSlothTeamA[slothTurnA]);
+            slothTurnB += 1;
+            if(slothTurnB == 4)
+            {
+                slothTurnB = 0;
+            }
+            
             /* If the variable is already true, it means that it is the turn for the enemy sloth.
             If we arrive here it will mean that the enemy sloth has finished his turn, and 
             it will change the variable to false.*/
         }
         else
         {
-            endTurnOfSloth = false;
-            DeactivateSloth(enemySloth, gunEnemySloth);
-            ActivateSloth(friendlySloth, gunFriendlySloth);
+            endTurnOfSloth = true;
+            DeactivateSloth(slothTeamA[slothTurnA], gunSlothTeamA[slothTurnA]);
+            ActivateSloth(slothTeamB[slothTurnB], gunSlothTeamB[slothTurnB]);
+            slothTurnA += 1;
+            if (slothTurnA == 4)
+            {
+                slothTurnA = 0;
+            }
 
         }
     }
--- SlothWars/Assets/Scripts/CreateSloth.cs
+++ SlothWars/Assets/Scripts/CreateSloth.cs
@@ -3,54 +3,94 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateSloth : MonoBehaviour {
-	GameObject cube;
-	ArrayList teamA;
-	ArrayList teamB;
+	public List<GameObject> teamA, gunsTeamA;
+	public List<GameObject> teamB, gunsTeamB;
 
-	GameObject allySloth;
-	GameObject enemySloth;
+
+	GameObject slothTeamA, gunSlothTeamA;
+	GameObject slothTeamB, gunSlothTeamB;
 
 	void Start(){
-		teamA = new ArrayList ();
-		teamB = new ArrayList ();
-		allySloth = GameObject.Find ("avatarFriendlySloth");
-		enemySloth = GameObject.Find ("avatarEnemySloth");
-	}
 
-	// Use this for initialization
-	public void CreateSlothTeamA () {
-		if (teamA.Count < 4) {
-			//cube =  GameObject.CreatePrimitive(PrimitiveType.Cube);
-			//cube.transform.position = new Vector3(teamA.Count+1, 0.5F, 0);
-			//teamA.Add (cube);
-			GameObject clone = Instantiate(allySloth);
-			clone.transform.position = new Vector3 (-teamA.Count - 1, 0.5F, 0);
-			teamA.Add (clone);
+		teamA = new List<GameObject>();
+        gunsTeamA = new List<GameObject>();
+
+		teamB = new List<GameObject>();
+        gunsTeamB = new List<GameObject>();
+
+        slothTeamA = GameObject.Find ("avatarSlothTeamA");
+        teamA.Add(slothTeamA);
+       
+        gunSlothTeamA = GameObject.Find("gunSlothTeamA");
+        gunsTeamA.Add(gunSlothTeamA);
+
+
+        slothTeamB = GameObject.Find ("avatarSlothTeamB");
+        teamB.Add(slothTeamB);
+
+        gunSlothTeamB = GameObject.Find("gunSlothTeamB");
+        gunsTeamB.Add(gunSlothTeamB);
+
+    }
+
+    // Use this for initialization
+    public void CreateSlothTeamA () {
+		if (teamA.Count < 4 && gunsTeamA.Count < 4)
+        {
+
+			GameObject newSlothTeamA = Instantiate(slothTeamA);
+            GameObject newGunTeamA = Instantiate(gunSlothTeamA);
+
+			newSlothTeamA.transform.position = new Vector3 (-teamA.Count - 1, 0.5F, 0);
+            newGunTeamA.transform.position = new Vector3(-gunsTeamA.Count - 1, 0.5F, 0);
+
+            teamA.Add (newSlothTeamA);
+            gunsTeamA.Add(newGunTeamA);
 		}
 
 	}
 
+/*
 	public void DestroySlothTeamA(){
-		cube = (GameObject) teamA [teamA.Count-1];
-		teamA.Remove (cube);
-		Destroy (cube);
+        if (teamA.Count == 0 && gunsTeamA.Count == 0)
+        {
+		    teamA.Remove((GameObject) teamA [teamA.Count-1]);
+            gunsTeamA.Remove((GameObject)gunsTeamA[gunsTeamA.Count - 1]);
+
+		    Destroy ((GameObject)teamA[teamA.Count - 1]);
+            Destroy ((GameObject)gunsTeamA[gunsTeamA.Count - 1]);
+
+        }
+    }
+*/
+    // Use this for initialization
+    public void CreateSlothTeamB () {
+		if (teamB.Count < 4 && gunsTeamB.Count < 4)
+        {
+
+			GameObject newSlothTeamB = Instantiate(slothTeamB);
+            GameObject newGunTeamB = Instantiate(gunSlothTeamB);
+
+			newSlothTeamB.transform.position = new Vector3 (+teamB.Count + 1, 0.5F, 0);
+            newGunTeamB.transform.position = new Vector3(-gunsTeamB.Count - 1, 0.5F, 0);
+
+            teamB.Add (newSlothTeamB);
+            gunsTeamB.Add(newGunTeamB);
+
+        }
+
 	}
+    /*
+    public void DestroySlothTeamB()
+    {
+        if (teamB.Count == 0 && gunsTeamB.Count == 0)
+        {           
+            teamB.Remove((GameObject)teamB[teamB.Count - 1]);
+            gunsTeamB.Remove((GameObject)gunsTeamB[gunsTeamB.Count - 1]);
 
-
-	// Use this for initialization
-	public void CreateSlothTeamB () {
-		if (teamB.Count < 4) {
-			GameObject clone = Instantiate(allySloth);
-			clone.transform.position = new Vector3 (+teamB.Count + 1, 0.5F, 0);
-			teamB.Add (clone);
-		}
-
-	}
-
-	public void DestroySlothTeamB(){
-		cube = (GameObject) teamB [teamB.Count-1];
-		teamB.Remove (cube);
-		Destroy (cube);
-	}
-
+            Destroy((GameObject)teamB[teamB.Count - 1]);
+            Destroy((GameObject)gunsTeamB[gunsTeamB.Count - 1]);
+        }
+    }
+    */
 }
