b1ae335eb68b82c2c81ec799a7482e681b234a86
silensrunners <ixent.traceur@gmail.com>
2017-10-15 06:38:47 +0200

    Add GameControl + Add team selection scene design + Add cool assets

---
+++ SlothWars/Assets/Scripts/GameControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameControl : MonoBehaviour {
+    
+    // GameControl instance that you may call from any other script in the scene using GameControl.control 
+    // and then you may access to any of its public variables and/or expressions.
+    public static GameControl control;
+
+    // List of 'sloths' of each team. List of strings, which are the type of sloth.
+    [HideInInspector]
+    public List<string> slothTeam1;
+    [HideInInspector]
+    public List<string> slothTeam2;
+
+    public readonly int maxTeamSloths = 4;
+
+    private void Awake()
+    {
+        if (control == null)
+        {
+            // if there is no GameControl created yet, makes it persistent between scenes and asigns it to itself
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            // if the next scene already has a GameControl object, destroys it and keeps the one from previous scenes
+            Destroy(gameObject);
+        }
+        else
+        {
+            // else, do nothing. Keeps the GameControl from previous scenes.
+        }
+    }
+
+    private void Start()
+    {
+        slothTeam1 = new List<string>();
+        slothTeam2 = new List<string>();
+    }
+}
