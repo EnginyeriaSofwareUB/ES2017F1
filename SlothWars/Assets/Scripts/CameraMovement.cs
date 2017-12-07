e48c45f61d7f3640e173f862e7f018ef94e57708
Albert Segura Santillana <segu.santi@gmail.com>
2017-12-07 11:32:33 +0100

    - Corrijo los problemas de la luz, que daban sombras muy feas. - Corrijo los problemas de la cámara, que no seguía a los sloths al moverse ni se centraba en un sloth al empezar la partida.

--- SlothWars/Assets/Scripts/CameraMovement.cs
+++ SlothWars/Assets/Scripts/CameraMovement.cs
@@ -26,8 +26,10 @@ public class CameraMovement : MonoBehaviour {
 	void Start () 
     {
 		turn = TurnController.Instance.GetActiveSloth ();
-		if (turn != null)
-			transform.position = new Vector3(turn.transform.position.x, 7.0f, cameraDistance);
+        if (turn != null)
+        {
+            transform.position = new Vector3(turn.transform.position.x, turn.transform.position.y + cameraHeight, cameraDistance);
+        }
 	}
 	
 	// Update is called once per frame
@@ -35,8 +37,14 @@ public class CameraMovement : MonoBehaviour {
     {
         projectile = GameObject.FindGameObjectsWithTag("projectile");
         explosion = GameObject.FindGameObjectsWithTag("explosion");
+        //print("IS MOVING: " + TurnController.Instance.GetActiveSloth().GetComponent<AnimPlayer>().IsMoving().ToString());
 
-        if (!TurnController.Instance.GetActiveSloth().GetComponent<AnimPlayer>().IsMoving()
+        if (TurnController.Instance.GetActiveSloth().GetComponent<AnimPlayer>().IsMoving())
+        {
+            transform.position = new Vector3(TurnController.Instance.GetActiveSloth().transform.position.x, 
+                TurnController.Instance.GetActiveSloth().transform.position.y + cameraHeight, cameraDistance);
+        }
+        else if (!TurnController.Instance.GetActiveSloth().GetComponent<AnimPlayer>().IsMoving()
             && !TurnController.Instance.GetActiveSloth().GetComponent<ShotScript>().GetShotLoad()
             && projectile.Length == 0
             && explosion.Length == 0)
@@ -52,11 +60,6 @@ public class CameraMovement : MonoBehaviour {
         {
             if(explosion.Length > 0)
                 transform.position = new Vector3(explosion[0].transform.position.x, projectile[0].transform.position.y, cameraDistance);
-        }
-        else if (TurnController.Instance.GetActiveSloth().GetComponent<AnimPlayer>().IsMoving())
-        {
-            transform.position = new Vector3(TurnController.Instance.GetActiveSloth().transform.position.x, 
-                TurnController.Instance.GetActiveSloth().transform.position.y + cameraHeight, cameraDistance);
         }
 
 		if (turn != TurnController.Instance.GetActiveSloth ()) 
