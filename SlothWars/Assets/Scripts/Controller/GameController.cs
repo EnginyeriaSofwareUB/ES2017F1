e5d2f8a8c3f6fcf73005b8e5cf1054a24e3a2409
Adrià <atorraag7@alumnes.ub.edu>
2017-12-17 18:51:14 +0100

    Con esto se acaba la issue #100. Añadido un nuevo icono para la representar la defensa

--- SlothWars/Assets/Scripts/Controller/GameController.cs
+++ SlothWars/Assets/Scripts/Controller/GameController.cs
@@ -94,7 +94,7 @@ public class GameController : MonoBehaviour {
 
 			HealthScript health = logic.AddComponent<HealthScript>();
             health.enabled = true;
-            GameObject healthBar = (GameObject)Instantiate(Resources.Load("ModelosDefinitivos/Prefabs/HealthBar"), logic.transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+            GameObject healthBar = (GameObject)Instantiate(Resources.Load("ModelosDefinitivos/Prefabs/HealthBarBlue"), logic.transform.position + new Vector3(0, 1, 0), Quaternion.identity);
             healthBar.GetComponent<RectTransform>().rotation = Quaternion.Euler(90, 0, 0);
             healthBar.transform.SetParent(logic.transform);
             health.SetHealthBar(healthBar);
@@ -149,7 +149,7 @@ public class GameController : MonoBehaviour {
 
      		HealthScript health = logic.AddComponent<HealthScript>();
             health.enabled = true;
-            GameObject healthBar = (GameObject)Instantiate(Resources.Load("ModelosDefinitivos/Prefabs/HealthBar"), logic.transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+            GameObject healthBar = (GameObject)Instantiate(Resources.Load("ModelosDefinitivos/Prefabs/HealthBarRed"), logic.transform.position + new Vector3(0, 1, 0), Quaternion.identity);
             healthBar.transform.SetParent(logic.transform);
 			//healthBar.GetComponent<RectTransform>().localRotation = Quaternion.Euler(90, 0, 0);
 
--- SlothWars/Assets/Scripts/HealthScript.cs
+++ SlothWars/Assets/Scripts/HealthScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class HealthScript : MonoBehaviour {
     public GameObject healthBar;
     private double health = 100;
-	private GameObject hp;
     private double maxHealth;
 	private GameObject shield;
 	private GameObject shieldEffect = null;
@@ -28,20 +27,9 @@ public class HealthScript : MonoBehaviour {
     public void SetHealthBar(GameObject healthBar){
         this.healthBar = healthBar;
     }
-    public void turnRight()
-    {
-        /*texthp.transform.eulerAngles = new Vector3(0, 0, 0);
-        texthp.transform.localPosition = new Vector3(0, 3, -0.5f); */
-    }
-    public void turnLeft()
-    {
-       /* texthp.transform.eulerAngles = new Vector3(0, 360, 0);
-        texthp.transform.localPosition = new Vector3(0, 3, 0.5f); */
-    }
 
 	public void setHealth(double health){
 		this.health = health;
-		//hp.GetComponent<TextMesh> ().text = "" + health;
 	}
 
 	public double getHealth(){
@@ -52,25 +40,28 @@ public class HealthScript : MonoBehaviour {
         this.maxHealth = maxHealth;
     }
 
-	public void UpdateHP(double hp,double shield)
+    public void UpdateHP(double hp, double shield)
     {
-		if (shield > 0) {
-			if (shieldEffect == null) {
-				this.shield.SetActive (true);
+        if (shield > 0)
+        {
+            if (shieldEffect == null)
+            {
+                this.shield.SetActive(true);
                 Debug.Log("shield");
-				shieldEffect = (GameObject)Instantiate (Resources.Load ("Objects/Shield"), this.transform.position+ new Vector3(0,0,-0.5f), Quaternion.identity);
-			}
-			this.shield.GetComponent<TextMesh> ().text = "" + shield;
-		} else if (shieldEffect != null) {
-			Destroy (shieldEffect);
-			shieldEffect = null;
-			this.shield.SetActive (false);
-		}
-        maxHealth = gameObject.GetComponent<Sloth>().GetMaxHp();
-        //Debug.Log(health);
-        //Debug.Log(health/maxHealth);
-        healthBar.GetComponent<HealthBarScript>().ChangeBarLevel(hp / maxHealth);
-        //this.hp.GetComponent<TextMesh>().text= "" + health;
-    }
+                shieldEffect = (GameObject)Instantiate(Resources.Load("Objects/Shield"), this.transform.position + new Vector3(0, 0, -0.5f), Quaternion.identity);
+            }
+            this.shield.GetComponent<TextMesh>().text = "" + shield;
 
+        }
+        else if (shieldEffect != null)
+        {
+            Destroy(shieldEffect);
+            shieldEffect = null;
+            this.shield.SetActive(false);
+        }
+        maxHealth = gameObject.GetComponent<Sloth>().GetMaxHp();
+        healthBar.GetComponent<HealthBarScript>().ChangeBarLevel(hp / maxHealth);
+        healthBar.GetComponent<HealthBarScript>().ChangeHealthText(hp);
+        healthBar.GetComponent<HealthBarScript>().ChangeTextShield(shield);
+    }
 }
