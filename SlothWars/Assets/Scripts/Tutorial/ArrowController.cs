2ffd1509400623b0095366c93f59dec01cc96848
Aliarachan <kaala_llunaa@hotmail.com>
2017-12-18 18:40:52 +0100

    red arrow y carpeta tutorial

---
+++ SlothWars/Assets/Scripts/Tutorial/ArrowController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+	private Vector3 target;
+	private bool isWorld;
+	private GameObject arrow;
+
+	// Use this for initialization
+	void Start () {
+		arrow = GameObject.Find("RedArrow");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public Vector3 getTarget(){
+		return target;
+	}
+
+	public void setTarget(Vector3 p){
+		target = p;
+	}
+
+	public void setIsWorld(bool b){
+		isWorld = b;
+	}
+
+	public void setArrowActive(bool b){
+		arrow.SetActive(b);
+	}
+}
