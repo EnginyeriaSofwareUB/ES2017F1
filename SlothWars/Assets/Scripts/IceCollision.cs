c854e8ff7df934ec9f560c75a33c4c6e3941d6f0
silensrunners <ixent.traceur@gmail.com>
2017-12-19 02:52:05 +0100

    Fix sloth falling into sloth + Add explosion push

--- SlothWars/Assets/Scripts/IceCollision.cs
+++ SlothWars/Assets/Scripts/IceCollision.cs
@@ -2,15 +2,20 @@
 
 public class IceCollision : MonoBehaviour {
 
+    private static bool first = true;
+
     public void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("COLLIDE 11111111111");
-        if ("Destroyable".Equals(collision.gameObject.tag))
+        if (first || "Destroyable".Equals(collision.gameObject.tag))
         {
-            Debug.Log("COLLIDE 222222222222");
+            first = false;
             Vector3 nextPos = gameObject.transform.position;
             nextPos = new Vector3(nextPos.x - 1, nextPos.y, nextPos.z);
             gameObject.transform.position = nextPos;
+        }
+        else
+        {
+            first = true;
         }
     }
 }
