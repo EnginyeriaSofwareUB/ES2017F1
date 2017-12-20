427413a7687fa93355829bb14fcd33aa76ac912c
silensrunners <ixent.traceur@gmail.com>
2017-12-20 02:02:58 +0100

    Refresh GameScreen scene

--- SlothWars/Assets/Scripts/GameUIScripts/ButtonOnMouseOver.cs
+++ SlothWars/Assets/Scripts/GameUIScripts/ButtonOnMouseOver.cs
@@ -24,7 +24,7 @@ public class ButtonOnMouseOver : MonoBehaviour, IPointerEnterHandler, IPointerEx
 	}
 
 	IEnumerator waiter(){
-		yield return new WaitForSeconds (0.7f);
+		yield return new WaitForSeconds (0.1f);
 		if (EventSystem.current.IsPointerOverGameObject())
             {
             show();
